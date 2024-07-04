34d4f9e580333d5ea466e357032b1ce9e8f8f727
CeHao-NUS <cehao@u.nus.edu>
2024-07-04 11:09:07 +0800

    lets try quick validation

--- scripts/hier/exp.sh
+++ scripts/hier/exp.sh
@@ -7,10 +7,25 @@ python scripts/hier/train_ll.py --config config.hier.maze2d_ll \
    --dataset maze2d-umaze-v1 
 
 
-# =================================
-python scripts/hier/plan_maze2d.py 
 
-# =================================
+
+#   ================================= hl + ll
+
+python scripts/hier/plan_maze2d.py \
+--conditional True --init_pose "(1, 1)" --target "(3,1)" --suffix "far"
+
+python scripts/hier/plan_maze2d.py \
+--conditional True --init_pose "(1, 1)" --target "(3,3)" --suffix "turn1"
+
+python scripts/hier/plan_maze2d.py \
+--conditional True --init_pose "(1, 1)" --target "(1,3)" --suffix "line"
+
+python scripts/hier/plan_maze2d.py \
+--conditional True --init_pose "(1, 1)" --target "(1,2)" --suffix "near"
+
+# ================================= hl test
+python scripts/hier/plan_maze2d_hl.py --config config.hier.maze2d_hl \
+  --dataset maze2d-umaze-v1 
 
 python scripts/hier/plan_maze2d_hl.py --config config.hier.maze2d_hl \
   --dataset maze2d-umaze-v1 \
