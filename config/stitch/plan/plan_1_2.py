34ccd1de12e866f9ffb437a614cd56e5ee52ecea
cehao <cehao@u.nus.edu>
2024-08-19 13:37:01 +0800

    results are ok

--- config/stitch/plan/plan_1_2.py
+++ config/stitch/plan/plan_1_2.py
@@ -1,5 +1,9 @@
 # # p-1_2: LL_varh + guide
 
 from config.stitch.plan.plan_1_1 import *
-base['plan']['value_loadpath'] = 'f:values/LL_varh_value_H{horizon}_T{n_diffusion_steps}_d{discount}_mH{min_horizon}'
-base['plan']['value_epoch'] = 'latest'
+
+base['plan'].update({
+    'value_loadpath': 'f:values/LL_varh_value_H{horizon}_T{n_diffusion_steps}_d{discount}_mH{min_horizon}',
+    'value_epoch': 'latest',
+    'prefix': 'plans/plan_1_2/',
+})
---
+++ scripts/maze_stitch/run/eval_for_p_1.sh
@@ -0,0 +1,19 @@
+# bash scripts/maze_stitch/run/eval_for_p_1.sh 
+
+# p-1_1
+# for init_pose in "(1,1.5)" "(1,1.1)" "(3,1)" "(3,2)" "(3,1.5)" "(3,3)" "(2,3)" "(1,3)" "(1,2)"
+# do
+#     python scripts/maze_stitch/plan/plan_1.py --config 'config.stitch.plan.plan_1_1' \
+#     --conditional True --init_pose $init_pose --target "(1, 1)" --suffix $init_pose &
+# done
+
+wait
+
+# p-1_2 
+for init_pose in "(1,1.5)" "(1,1.1)" "(3,1)" "(3,2)" "(3,1.5)" "(3,3)" "(2,3)" "(1,3)" "(1,2)"
+do
+    python scripts/maze_stitch/plan/plan_1.py --config 'config.stitch.plan.plan_1_2' \
+    --conditional True --init_pose $init_pose --target "(1, 1)" --suffix $init_pose &
+done
+
+wait
