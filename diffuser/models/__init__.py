145c44e96414ed2c65657ee7ad895074cd1ea2bc
cehao <cehao@u.nus.edu>
2024-10-09 22:36:46 +0800

    test noised

--- diffuser/models/__init__.py
+++ diffuser/models/__init__.py
@@ -2,4 +2,5 @@ from .temporal import TemporalUnet, ValueFunction
 from .diffusion import GaussianDiffusion, ValueDiffusion
 from .rope_diffusion import RopeDiffusion
 from .stitch_diffsuion import BatchGaussianDiffusion, BatchValueDiffusion
+from .coupled_diffusion import CoupledGaussianDiffusion, CoupledGaussianDiffusion_ForwardNoise
 from .helpers import *
--- scripts/single/tasks.sh
+++ scripts/single/tasks.sh
@@ -3,20 +3,19 @@
 
 # train diffusion
 python scripts/train/train_diffuser.py --config 'config.single.train_diff'  --dataset maze2d-umaze-v1 --device "cuda"
-
-
 # plan traj
 python scripts/single/plan.py --config 'config.single.plan_diff'  --dataset maze2d-umaze-v1 --device "cuda"
 
 
-
+# med, large
 python scripts/train/train_diffuser.py --config 'config.single.train_diff'  --dataset maze2d-medium-v1 --device "cuda:1"
 python scripts/train/train_diffuser.py --config 'config.single.train_diff'  --dataset maze2d-large-v1 --device "cuda:2"
 
 
-python scripts/single/plan.py --config 'config.single.plan_diff'  --dataset maze2d-medium-v1 --device "cuda"
-
-
-# test
+# test big
 python scripts/train/train_diffuser.py --config 'config.single.train_diff_test'  --dataset maze2d-testbig-v0 --device "cuda:7"
 python scripts/single/plan.py --config 'config.single.plan_diff_test'  --dataset maze2d-testbig-v0 --device "cuda:0"
+
+
+#  CoupledGaussianDiffusion_ForwardNoise
+python scripts/train/train_diffuser.py --config 'config.single.train_diff_coupled_forwardnoise'  --dataset maze2d-umaze-v1 --device "cuda"
